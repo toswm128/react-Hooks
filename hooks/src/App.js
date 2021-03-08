5e3ff8dd5eb1dc6861e201f134a463e6add2b463
toswm128 <chominsu10@gmail.com>
2021-03-08 19:23:55 +0900

    [ADD] UseInput two

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,16 +1,26 @@
 import React,{useState} from "react"
 
 
-const useInput = (initialValue) =>{
-  const [value, setValue] = useState(initialValue)
-  const onChange = (e) =>{
-    console.log(e.target)
-  }
+const useInput = (initialValue,vaildator) =>{
+  const [value, setValue] = useState(initialValue);
+  const onChange = event =>{
+    const {
+      target:{value}
+    }= event;
+    let willUpdate = true;
+    if(typeof vaildator ==="function"){
+      willUpdate = vaildator(value)
+    }
+    if(willUpdate){
+      setValue(value)
+    }
+  };
   return { value, onChange }
 }
 
 function App()   {
-  const name = useInput("Mr.")
+  const maxLen = value => !value.includes("@")
+  const name = useInput("Mr.",maxLen)
   return (
     <>
       <div>
