16bf6c46aa6376805d96a8858d6022c437ac64c6
toswm128 <chominsu10@gmail.com>
2021-03-19 10:32:08 +0900

    [ADD] useTitle

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,38 +1,28 @@
-import React,{useState} from "react"
+import React,{useState, useEffect} from "react"
 
-
-const content = [
-  {
-    tap: "1번",
-    content: "1번에 들어갈거",
-  },
-  {
-    tap: "2번",
-    content: "2번에 들어갈꺼",
+const useTitle = (initialValue) =>{
+  const [value, setValue] = useState(initialValue)
+  const updateTitle = () =>{
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = value
   }
-]
+  useEffect(updateTitle,[value]);
+  return setValue
+}
 
-const useTabs = (initialTab,allTabs) =>{
-  const [currentIndex, setCurrentIndex] = useState(initialTab);
-  if(!allTabs || !Array.isArray(allTabs)){
-    return;
-  }
-  return{
-    currentItem:allTabs[currentIndex],
-    changeItem:setCurrentIndex
-  };
-};
 
 function App()   {
-  const {currentItem,changeItem} = useTabs(0,content)
+  const titleUpdate = useTitle("Looding")
+  setTimeout(() => {
+    titleUpdate("HOME")
+  }, 1000);
   return (
     <>
       <div>
         <h1>hi</h1>
-        {content.map((section,index)=>
-           <button onClick={()=>changeItem(index)}>{section.tap}</button>)}
+        
       </div>
-      <div>{currentItem.content}</div>
+      
     </>
   );
 }
