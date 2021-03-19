494a0c7585cd92a64090701b99ca7993e7796604
toswm128 <chominsu10@gmail.com>
2021-03-19 10:10:41 +0900

    [ADD]useTab

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,33 +1,38 @@
 import React,{useState} from "react"
 
 
-const useInput = (initialValue,vaildator) =>{
-  const [value, setValue] = useState(initialValue);
-  const onChange = event =>{
-    const {
-      target:{value}
-    }= event;
-    let willUpdate = true;
-    if(typeof vaildator ==="function"){
-      willUpdate = vaildator(value)
-    }
-    if(willUpdate){
-      setValue(value)
-    }
+const content = [
+  {
+    tap: "1번",
+    content: "1번에 들어갈거",
+  },
+  {
+    tap: "2번",
+    content: "2번에 들어갈꺼",
+  }
+]
+
+const useTabs = (initialTab,allTabs) =>{
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  if(!allTabs || !Array.isArray(allTabs)){
+    return;
+  }
+  return{
+    currentItem:allTabs[currentIndex],
+    changeItem:setCurrentIndex
   };
-  return { value, onChange }
-}
+};
 
 function App()   {
-  const maxLen = value => !value.includes("@")
-  const name = useInput("Mr.",maxLen)
+  const {currentItem,changeItem} = useTabs(0,content)
   return (
     <>
       <div>
         <h1>hi</h1>
-        <input type="text" placeholder="Name" {...name}  />
+        {content.map((section,index)=>
+           <button onClick={()=>changeItem(index)}>{section.tap}</button>)}
       </div>
-      
+      <div>{currentItem.content}</div>
     </>
   );
 }
